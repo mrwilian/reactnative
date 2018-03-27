27bdceb53a649eecb768d89fc38209e2c5dd4eed
mrwilian <37774404+mrwilian@users.noreply.github.com>
2018-03-27 13:34:43 +0800

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import {
   StyleSheet, Text,
   View,
 } from 'react-native';
-//import Layout from './src/latihan/Layout'
+import Layout from './src/latihan/Layout'
+import Layout from './src/latihan/VolumeBalok'
 import Layout from './src/Cuaca/PrakiraanCuaca'
 export default class App extends Component {
   render(){
